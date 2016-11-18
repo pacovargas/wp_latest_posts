ffc5c8f234b32ffe6d9b63bee816cae960c7495e
fvargasestrada <paco@sauz.es>
2016-11-19 00:36:50 +0100

    prueba de cabecera de licencia

--- views/js/back.js
+++ views/js/back.js
@@ -1,3 +1,16 @@
+/**
+* NOTICE OF LICENSE
+*
+* This file is licenced under the GNU General Public License v3.0.
+* With the purchase or the installation of the software in your application
+* you accept the licence agreement.
+*
+*
+*  @author    Francisco Javier Vargas
+*  @copyright 2010-2016 Sauz Web Solutions
+*  @license   LICENSE.txt
+*/
+
 function validarUrl(url){
     var reg = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \?=.-]*)*\/?$/;
     return reg.test(url);
