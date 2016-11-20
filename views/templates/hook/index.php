74b0c85a88567cd8a69221858ecc226f8406d1a1
fvargasestrada <paco@sauz.es>
2016-11-20 13:24:31 +0100

    faltaba un index.php

---
+++ views/templates/hook/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* NOTICE OF LICENSE
+*
+* This file is licenced under the GNU General Public License v3.0.
+* With the purchase or the installation of the software in your application
+* you accept the licence agreement.
+*
+*  @author    Francisco Javier Vargas
+*  @copyright 2010-2016 Sauz Web Solutions
+*  @license   LICENSE.txt
+*/
+
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+
+header('Location: ../');
+exit;
